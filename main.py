01a31f32d497b69b1391f205064fc5c688da63ec
AdinAdryTjindarbumi <167547879+AdinAdryTjindarbumi@users.noreply.github.com>
2024-05-20 18:21:27 +0700

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ class Game:
         self.screen_height = height
 
         #background yang digunakan
-        self.background = pygame.image.load('Assets/Map/7.png')
+        self.background = pygame.image.load('Assets/Map/map.png')
 
         #Background music
         pygame.mixer.music.load("Assets/Sound/Space Sprinkles.mp3")
